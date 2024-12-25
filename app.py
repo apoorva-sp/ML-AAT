5847c16fab2e6ace42739c95049dd9edce170d83
Aditya-Natarajan <aditya.natarajan1@gmail.com>
2024-12-25 11:49:09 +0530

    final changes made to html and app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from random import uniform
 from main import pre_processing, linear_regression, decision_tree, random_forest, evaluate, encode, random_traffic, \
     random_weather
 from path import path, api_key
+from pre_processing import eval_models_with_pca
 
 gmaps = googlemaps.Client(key=api_key)
 app = Flask(__name__)
@@ -14,6 +15,7 @@ app = Flask(__name__)
 models = []
 performances = []
 performances_measure = []
+names = ["Linear Regression", "Decision Tree CART", "Random Forest"]
 
 df = pd.read_csv(path)
 x_train, x_test, y_train, y_test = pre_processing(df)
@@ -21,17 +23,20 @@ x_train, x_test, y_train, y_test = pre_processing(df)
 linear_regression_model = linear_regression(x_train, x_test, y_train)
 decision_tree_model = decision_tree(x_train, x_test, y_train)
 random_forest_model = random_forest(x_train, x_test, y_train)
+evaluated_with_pca = eval_models_with_pca()
 
 models.extend([linear_regression_model[0], decision_tree_model[0], random_forest_model[0]])
 performances.extend([linear_regression_model[1], decision_tree_model[1], random_forest_model[1]])
 
-for p in performances:
-    performances_measure.append(evaluate(y_test, p))
+for p in range(3):
+    performances_measure.append([names[p], evaluate(y_test, performances[p])])
+
+for i in range(3):
+    performances_measure.append([names[i] + " with PCA", evaluated_with_pca[i]])
 
 
 @app.route('/')
 def root():
-    print(performances_measure)
     return render_template('home.html')
 
 
@@ -43,10 +48,11 @@ def performance():
 @app.route('/predicted', methods=["POST"])
 def home():
     now = datetime.now()
-    directions_result = gmaps.directions("kamakshipalya bangalore karnataka",
-                                         "elimale karnataka",
+    directions_result = gmaps.directions(request.form['source'],
+                                         request.form['destination'],
                                          mode="driving",
                                          departure_time=now)
+
     distance = directions_result[0]["legs"][0]["distance"]["value"] / 1000
     duration = directions_result[0]["legs"][0]["duration"]["value"] / 60
     data = {
@@ -65,8 +71,8 @@ def home():
     training_columns = x_train.columns
     new_data = encode(user_data_frame).reindex(columns=training_columns, fill_value=0)
     predicted_fare = 0
-    for i in range(3):
-        predicted_fare += models[i].predict(new_data)[0]
+    for m in range(3):
+        predicted_fare += models[m].predict(new_data)[0]
     predicted_fare = round(predicted_fare / 3, 2)
     if int(data['Trip_Distance_km'][0]) == 0 or int(data['Trip_Duration_Minutes'][0]) == 0:
         predicted_fare = 0
